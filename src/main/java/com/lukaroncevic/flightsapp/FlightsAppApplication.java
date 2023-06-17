662cf0353efa8c8db1f71fcaeeba1f9ec477266b
lroncevic <roncevicluka@outlook.com>
2023-06-17 10:20:45 +0200

    Initial project add

---
+++ src/main/java/com/lukaroncevic/flightsapp/FlightsAppApplication.java
@@ -0,0 +1,13 @@
+package com.lukaroncevic.flightsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lukaroncevic/flightsapp/FlightsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukaroncevic.flightsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
